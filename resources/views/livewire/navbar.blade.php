11489171897f12b5cca9eab49dd66ea78d2f2ba2
Santiago <santiagobinario@gmail.com>
2024-11-26 03:16:07 +0100

    update

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -17,9 +17,9 @@
 
                 <!-- Menú de navegación - Desktop -->
                 <div class="hidden md:flex space-x-8 items-center">
-                    <a href="#" class="text-sm font-medium px-3 py-2 transition-all hover:text-blue-500 hover:scale-105 transform duration-200 ease-in-out">Home</a>
-                    <a href="#" class="text-sm font-medium px-3 py-2 transition-all hover:text-blue-500 hover:scale-105 transform duration-200 ease-in-out">About</a>
-                    <a href="#" class="text-sm font-medium px-3 py-2 transition-all hover:text-blue-500 hover:scale-105 transform duration-200 ease-in-out">Services</a>
+                    <a href="#" class="text-sm font-medium px-3 py-2 transition-all hover:text-blue-500 hover:scale-105 transform duration-200 ease-in-out">Walles</a>
+                    <a href="#" class="text-sm font-medium px-3 py-2 transition-all hover:text-blue-500 hover:scale-105 transform duration-200 ease-in-out">My Walles</a>
+                    <a href="#" class="text-sm font-medium px-3 py-2 transition-all hover:text-blue-500 hover:scale-105 transform duration-200 ease-in-out">Portfolios</a>
                 </div>
 
                 <!-- Iconos de conectar cartera y seleccionar red -->
@@ -44,9 +44,9 @@
             </button>
         </div>
         <div class="flex flex-col items-center space-y-4 mt-8">
-            <a href="#" class="text-xl text-gray-600 hover:text-blue-500">Home</a>
-            <a href="#" class="text-xl text-gray-600 hover:text-blue-500">About</a>
-            <a href="#" class="text-xl text-gray-600 hover:text-blue-500">Services</a>
+            <a href="#" class="text-xl text-gray-600 hover:text-blue-500">Walles</a>
+            <a href="#" class="text-xl text-gray-600 hover:text-blue-500">My walles</a>
+            <a href="#" class="text-xl text-gray-600 hover:text-blue-500">Portfolios</a>
         </div>
     </div>
 

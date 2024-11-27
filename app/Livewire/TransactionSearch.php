0b9db517770dc4481bdae928cc4b006808ad4fb9
Santiago <santiagobinario@gmail.com>
2024-11-27 21:36:51 +0100

    update

--- app/Livewire/TransactionSearch.php
+++ app/Livewire/TransactionSearch.php
@@ -13,6 +13,7 @@ class TransactionSearch extends Component
     public $currentPage = 1;
     public $pageSize = 10; // Número de transacciones por página
     public $totalTransactions = 0;
+    public $balance; // Para almacenar el saldo de la dirección
 
     public function searchTransactions()
     {
@@ -25,7 +26,7 @@ class TransactionSearch extends Component
             return;
         }
 
-        // Llamada a la API de Etherscan
+        // Llamada a la API de Etherscan para obtener transacciones
         $apiKey = env('ETHERSCAN_API_KEY'); // Configura tu clave en .env
         $response = Http::get('https://api.etherscan.io/api', [
             'module' => 'account',
@@ -46,6 +47,39 @@ class TransactionSearch extends Component
             $this->totalTransactions = count($data['result']); // Etherscan no devuelve total real en la API gratuita
         } else {
             $this->errorMessage = 'No se pudieron recuperar las transacciones o la dirección no tiene transacciones.';
+        }
+    }
+
+    // Función para obtener el saldo de la dirección
+    public function getBalance()
+    {
+        $this->errorMessage = null; // Reset error message
+        $this->balance = null; // Reset balance
+
+        // Validar la dirección Ethereum
+        if (!preg_match('/^0x[a-fA-F0-9]{40}$/', $this->address)) {
+            $this->errorMessage = 'La dirección Ethereum no es válida.';
+            return;
+        }
+
+        // Llamada a la API de Etherscan para obtener el saldo
+        $apiKey = env('ETHERSCAN_API_KEY'); // Configura tu clave en .env
+        $response = Http::get('https://api.etherscan.io/api', [
+            'module' => 'account',
+            'action' => 'balance',
+            'address' => $this->address,
+            'tag' => 'latest',
+            'apikey' => $apiKey,
+        ]);
+
+        $data = $response->json();
+
+        if ($data['status'] == '1' && isset($data['result'])) {
+            // El saldo está en Wei, convertirlo a ETH
+            $weiBalance = $data['result'];
+            $this->balance = $weiBalance / 1e18; // Convertir de Wei a ETH
+        } else {
+            $this->errorMessage = 'No se pudo obtener el saldo de la dirección.';
         }
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= env('APP_NAME');?></title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Estilos -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-    @vite('resources/css/app.css')
+
+    <!-- Laravel Vite -->
+    @vite(['resources/css/app.css'])
+
+    <!-- Livewire -->
     @livewireStyles
 </head>
-        @livewire('navbar')
-        <br>
-        <br>
-        <br>
-        @livewire('transaction-search')
-    @vite('resources/js/app.js')
+<body>
+    <!-- Navbar -->
+    @livewire('navbar')
+    <br><br><br>
+
+    <!-- Contenido Principal -->
+    @livewire('transaction-search')
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
     @livewireScripts
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </body>
 </html>
